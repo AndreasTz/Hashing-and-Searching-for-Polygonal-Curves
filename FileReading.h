7be5e89d801ee72f8d87ada717c8f9b76b4776ca
Popi032 <32679553+Popi032@users.noreply.github.com>
2017-10-21 00:17:37 +0300

    Add files via upload

---
+++ FileReading.h
@@ -0,0 +1,31 @@
+#ifndef FILEREADING__H
+#define FILEREADING__H
+
+#include "Hashing.h"
+#include "HelpClasses.h"
+
+extern double R;
+extern double dimension;
+
+class InitialCurve{
+  public:
+
+	    string curve_id;
+	    int noofPointsInCurve;
+	    double ** curvePoints;
+
+	    InitialCurve(){
+
+        this -> curvePoints = new double*[this -> noofPointsInCurve];
+        for(int i = 0; i < this -> noofPointsInCurve; i++){
+            this -> curvePoints[i] = new double[(int)dimension];
+          }
+      }
+};
+
+
+void readingFromFile(string Filename , HashMap ** const HashArray,PreferedDetails * const details);
+
+
+
+#endif
