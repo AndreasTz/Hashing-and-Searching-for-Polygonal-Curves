6d9cf9f52cde5b27b6c1c7098ae87b91907abe63
AndreasTz <32654863+AndreasTz@users.noreply.github.com>
2017-10-26 15:03:30 +0300

    Update frechet.cpp

--- frechet.cpp
+++ frechet.cpp
@@ -26,3 +26,28 @@ double PointDistance(vector<double>* v1, vector<double>* v2, int point1, int poi
     cout << distance << "****!" << endl;
 	return distance;
 }
+
+double FrechetDistance(vector<double>* v1, vector<double>* v2, int d)
+{   
+    double** C = new double*[v1->size()];
+    for(int i = 0; i < v1->size(); ++i)
+        C[i] = new double[v2->size()];
+    //Initialize
+    C[0][0] = PointDistance(v1, v2, 0, 0, d);
+    //Dynamic Filling the n*m double array
+    for (int i = 0; i < v1->size(); i++)
+    {
+        for (int j = 0; j < v2->size(); j++)
+        {
+            if (i == 0 && j == 0) continue;
+            if (i == 0) C[0][j] = max(C[0][j-1], PointDistance(v1, v2, 0, j, d));
+            else if (j == 0) C[i][0] = max(C[i-1][0], PointDistance(v1, v2, i, 0, d));
+            else
+            {
+                double temp = min(C[i-1][j], C[i-1][j-1]);
+                C[i][j] = max(min(temp, C[i][j-1]), PointDistance(v1, v2, i, j, d));
+            }
+        }
+    }
+    return C[v1->size()][v2->size()];
+}
